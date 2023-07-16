1deb941a974291608120812fdb65e00769e14171
HumanBox <stashksenia@gmail.com>
2023-07-16 12:53:37 +0300

    test ok

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,46 @@
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Controller {
-    public static AtomicInteger counter = new AtomicInteger();
+    public AtomicInteger counter = new AtomicInteger();
     //time
-    // public chooseYourFighter
+    // chooseYourFighter
     // comparator
-    // printArray
+    protected int[] smallArr = new int[50];
+    protected int[] bigArr = new int[15000];
+
+    Sorts sorts = new Sorts();
+
+    private void fillArr(int[] array){
+        for(int i = 0; i < array.length; i++){
+            array[i] = new Random().nextInt(101);
+        }
+    }
+    private int[] getSmallArray(){
+        fillArr(smallArr);
+        return this.smallArr;
+    }
+
+    private int[] getBigArray(){
+        fillArr(bigArr);
+        return this.bigArr;
+    }
+
+    private void printArray(){
+        for (int element: this.smallArr) {
+            System.out.printf("%d ", element);
+        }
+        System.out.println();
+    }
+    public void start(){
+        getSmallArray();
+        getBigArray();
+        printArray();
+        sorts.bubbleSort(smallArr);
+        printArray();
+    }
+
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
+
+        Controller battle = new Controller();
+        battle.start();
         // startSortBattle
         // printArray - yes no
         }

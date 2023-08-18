5a82af348afa9b71ff2325d6e450169a523d5e74
HumanBox <stashksenia@gmail.com>
2023-08-18 20:05:10 +0300

    some kind of comparator

---
+++ src/CompareSorts.java
@@ -0,0 +1,29 @@
+import sortCollection.Sorts;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class CompareSorts {
+
+    ArrayList<Sorts> array;
+
+    public CompareSorts(ArrayList<Sorts> data){
+        this.array = data;
+    }
+
+    public void winner(){
+        Collections.sort(
+                this.array,
+                (t0, t1) -> {
+                    if (t0.getTimer() > t1.getTimer())
+                        return 1;
+                    if (t0.getTimer() < t1.getTimer())
+                        return -1;
+                    if(t0.getTimer() == t1.getTimer()){
+                        return t0.getCounter() - t1.getCounter();
+                    }
+                    return 0;
+                });
+    }
+}
